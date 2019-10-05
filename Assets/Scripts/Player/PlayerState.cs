8787b2126118106da029bdf0b04b132f1dcba619
DzepaDC <starokozhev@bk.ru>
2019-10-05 16:10:24 +0700

    add some

---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour {
+
+    public int hungerLevel = 0;
+    public int maxHungerLevel = 100;
+    
+    public int tempLevel = 0;
+    public int maxTemp = 100;
+    public int minTemp = -100;
+    
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
